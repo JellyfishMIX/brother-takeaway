35f9a9e839a2e4f8a16abf9eca449a79fea7a3c5
JellyfishMIX <2273516339@qq.com>
2019-12-26 19:59:10 +0800

    :tada: Initial commit

---
+++ src/main/java/me/jmix/brothertakeaway/BrotherTakeawayApplication.java
@@ -0,0 +1,13 @@
+package me.jmix.brothertakeaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrotherTakeawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrotherTakeawayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jmix/brothertakeaway/BrotherTakeawayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jmix.brothertakeaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrotherTakeawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
