c125fcce1f09ae98b3582767c051a4974f4467b5
JellyfishMIX <2273516339@qq.com>
2019-12-28 23:11:30 +0800

    :pencil: Add a comment in the ProductCategoryRepository of the dao layer

--- src/main/java/me/jmix/brothertakeaway/dao/ProductCategoryRepository.java
+++ src/main/java/me/jmix/brothertakeaway/dao/ProductCategoryRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+    // 此方法的名字格式特定
     List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
 }
