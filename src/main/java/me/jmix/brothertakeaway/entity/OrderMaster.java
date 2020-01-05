f0b064e410993dd754aba93d9dc39abfe65650f4
JellyfishMIX <2273516339@qq.com>
2020-01-05 23:54:46 +0800

    :construction: Add OrderMaster entity

---
+++ src/main/java/me/jmix/brothertakeaway/entity/OrderMaster.java
@@ -0,0 +1,48 @@
+package me.jmix.brothertakeaway.entity;
+
+import lombok.Data;
+import me.jmix.brothertakeaway.enums.OrderMasterStateEnum;
+import me.jmix.brothertakeaway.enums.PayStateEnum;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "tb_order_master")
+@Entity
+@Data
+@DynamicInsert
+@DynamicUpdate
+public class OrderMaster {
+    // 订单Id
+    private String orderId;
+
+    // 顾客名字
+    private String customerName;
+
+    // 顾客手机号
+    private String customerPhone;
+
+    // 顾客地址
+    private String customerAddress;
+
+    // 顾客微信openid
+    private String customerOpenid;
+
+    // 订单总金额
+    private String orderAmount;
+
+    // 订单状态，默认为0: 新下单
+    private Integer orderStatus = OrderMasterStateEnum.NEW.getStateCode();
+
+    // 支付状态，默认为0: 未支付
+    private Integer payStatus = PayStateEnum.WAIT.getStateCode();
+
+    // 创建时间
+    private Date createTime;
+
+    // 修改时间
+    private Date updateTime;
+}
---
+++ src/main/java/me/jmix/brothertakeaway/enums/OrderMasterStateEnum.java
@@ -0,0 +1,18 @@
+package me.jmix.brothertakeaway.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderMasterStateEnum {
+    NEW(0, "新订单"),
+    FINISHED(1, "完结"),
+    CANCEL(2, "已取消");
+
+    private Integer stateCode;
+    private String stateInfo;
+
+    OrderMasterStateEnum(Integer stateCode, String stateInfo) {
+        this.stateCode = stateCode;
+        this.stateInfo = stateInfo;
+    }
+}
