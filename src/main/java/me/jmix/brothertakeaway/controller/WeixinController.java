0104876ef45ad0341772c9e071b1ed1b964f9da2
JellyfishMIX <2273516339@qq.com>
2020-01-18 23:36:31 +0800

    :construction: Update the WeixinController, now it can take the redirect code

--- src/main/java/me/jmix/brothertakeaway/controller/WeixinController.java
+++ src/main/java/me/jmix/brothertakeaway/controller/WeixinController.java
@@ -3,6 +3,7 @@ package me.jmix.brothertakeaway.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class WeixinController {
     @GetMapping("/auth")
-    public void auth() {
+    public void auth(@RequestParam("code") String code) {
         log.info("进入auth的方法");
+        log.info("code = {}", code);
     }
 }
