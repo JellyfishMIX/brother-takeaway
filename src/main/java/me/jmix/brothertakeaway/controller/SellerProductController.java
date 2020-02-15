0bfce033e19929c92dfa43cbf3536bb6686f20d1
JellyfishMIX <2273516339@qq.com>
2020-02-15 22:10:04 +0800

    :construction: Add a method "index" in the SellerProductController

--- src/main/java/me/jmix/brothertakeaway/controller/SellerProductController.java
+++ src/main/java/me/jmix/brothertakeaway/controller/SellerProductController.java
@@ -1,19 +1,22 @@
 package me.jmix.brothertakeaway.controller;
 
-import me.jmix.brothertakeaway.dto.OrderDTO;
+import me.jmix.brothertakeaway.entity.ProductCategory;
 import me.jmix.brothertakeaway.entity.ProductInfo;
 import me.jmix.brothertakeaway.exception.service.ProductServiceException;
+import me.jmix.brothertakeaway.service.ProductCategoryService;
 import me.jmix.brothertakeaway.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -21,6 +24,8 @@ import java.util.Map;
 public class SellerProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductCategoryService productCategoryService;
 
     @GetMapping("/list")
     public ModelAndView list(@RequestParam(value = "page", defaultValue = "1") Integer page,
@@ -67,5 +72,20 @@ public class SellerProductController {
         map.put("url", "/sell/seller/product/list");
 
         return new ModelAndView("common/success", map);
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index(@RequestParam(value = "productId", required = false) String productId) {
+        Map<String, Object> map = new HashMap<>();
+        if (!StringUtils.isEmpty(productId)) {
+            ProductInfo productInfo = productService.getProductInfoByProductId(productId);
+            map.put("productInfo", productInfo);
+        }
+
+        // 查询所有商品的类目
+        List<ProductCategory> productCategoryList = productCategoryService.getAllProductCategory();
+        map.put("productCategoryList", productCategoryList);
+
+        return new ModelAndView("product/index", map);
     }
 }
