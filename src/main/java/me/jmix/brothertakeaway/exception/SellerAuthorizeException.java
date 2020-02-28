e187afcc920f7fd5185c70ed44ee603e1c15d934
JellyfishMIX <2273516339@qq.com>
2020-02-28 18:21:32 +0800

    :construction: Added the SellerAuthorizeAspect

---
+++ src/main/java/me/jmix/brothertakeaway/exception/SellerAuthorizeException.java
@@ -0,0 +1,5 @@
+package me.jmix.brothertakeaway.exception;
+
+public class SellerAuthorizeException extends RuntimeException {
+    private static final long serialVersionUID = 14468480243316383L;
+}
---
+++ src/main/java/me/jmix/brothertakeaway/handler/SellerExceptionHandler.java
@@ -0,0 +1,16 @@
+package me.jmix.brothertakeaway.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class SellerExceptionHandler {
+    // @Autowired
+    //
+    // // 拦截登录异常
+    // public ModelAndView handlerAuthorException() {
+    //     return new ModelAndView("redirect:".concat());
+    // }
+
+}
