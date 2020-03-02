b01a8b9288940ec94b58c22fc4b695e9f6e28116
JellyfishMIX <2273516339@qq.com>
2020-03-02 22:53:36 +0800

    :construction: Added WebSocket service

---
+++ src/main/java/me/jmix/brothertakeaway/config/WebsocketConfig.java
@@ -0,0 +1,15 @@
+package me.jmix.brothertakeaway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Component
+@Slf4j
+public class WebsocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
---
+++ src/main/java/me/jmix/brothertakeaway/service/WebSocket.java
@@ -0,0 +1,25 @@
+package me.jmix.brothertakeaway.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+@ServerEndpoint("/webSocket")
+@Slf4j
+public class WebSocket {
+    private Session session;
+
+    private static CopyOnWriteArraySet<WebSocket> webSocketSet = new CopyOnWriteArraySet<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        webSocketSet.add(this);
+        log.info("【websocket消息】有新的连接，总数：{}", webSocketSet.size());
+    }
+}
