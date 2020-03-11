50d82ef3132d21f0573c4db39cdbf56373ea91ea
JellyfishMIX <2273516339@qq.com>
2020-03-11 23:45:26 +0800

    :construction: Added the ProductCategoryMapper

---
+++ src/main/java/me/jmix/brothertakeaway/dao/mapper/ProductCategoryMapper.java
@@ -0,0 +1,17 @@
+package me.jmix.brothertakeaway.dao.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+public interface ProductCategoryMapper {
+    /**
+     * 通过Map插入
+     * 返回值为插入的条数
+     * @param map
+     * @return
+     */
+    @Insert("insert into product_category(category_name, category_type) values (#{categoryName, jdbcType=VARCHAR}, #{category_type, categoryType, jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+}
