0dab348637a82c60512d6e910b6bbbaa1f18ef27
JellyfishMIX <2273516339@qq.com>
2020-03-12 23:44:17 +0800

    :construction: Update the ProductCategoryMapper with @Insert, @Select, @Update. The test has passed.

--- src/main/java/me/jmix/brothertakeaway/dao/mapper/ProductCategoryMapper.java
+++ src/main/java/me/jmix/brothertakeaway/dao/mapper/ProductCategoryMapper.java
@@ -1,9 +1,13 @@
 package me.jmix.brothertakeaway.dao.mapper;
 
-import org.apache.ibatis.annotations.Insert;
+import me.jmix.brothertakeaway.entity.ProductCategory;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
+@Component(value = "productCategoryMapper")
 public interface ProductCategoryMapper {
     /**
      * 通过Map插入
@@ -13,4 +17,44 @@ public interface ProductCategoryMapper {
      */
     @Insert("insert into tb_product_category(category_name, category_type) values (#{categoryName, jdbcType=VARCHAR}, #{categoryType, jdbcType=INTEGER})")
     int insertByMap(Map<String, Object> map);
+
+    /**
+     * 通过Object插入
+     * 返回值为插入的条数
+     * @param productCategory
+     * @return
+     */
+    @Insert("insert into tb_product_category(category_name, category_type) values (#{categoryName, jdbcType=VARCHAR}, #{categoryType, jdbcType=INTEGER})")
+    int insertByObject(ProductCategory productCategory);
+
+    /**
+     * 通过categoryType查询productCategory
+     * 由于categoryType为constraint key, 所以结果返回单条
+     * @param categoryType
+     * @return
+     */
+    @Select("select * from tb_product_category where category_type = #{categoryType}")
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "category_type", property = "categoryType")
+    })
+    ProductCategory findByCategoryType(Integer categoryType);
+
+    /**
+     * 通过categoryName查询productCategory
+     * 由于categoryName可能出现重复，所以结果返回List
+     * @param categoryName
+     * @return
+     */
+    @Select("select * from tb_product_category where category_name = #{categoryName}")
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "category_type", property = "categoryType")
+    })
+    List<ProductCategory> findByCategoryName(String categoryName);
+
+    @Update("update tb_product_category set category_name = #{categoryName} where category_type = #{categoryType}")
+    int updateByCategoryType(String categoryName, Integer categoryType);
 }
--- src/test/java/me/jmix/brothertakeaway/dao/mapper/ProductCategoryMapperTest.java
+++ src/test/java/me/jmix/brothertakeaway/dao/mapper/ProductCategoryMapperTest.java
@@ -1,21 +1,23 @@
 package me.jmix.brothertakeaway.dao.mapper;
 
+import me.jmix.brothertakeaway.entity.ProductCategory;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductCategoryMapperTest {
-    @Autowired
+    @Resource
     private ProductCategoryMapper productCategoryMapper;
 
     @Test
@@ -25,6 +27,41 @@ class ProductCategoryMapperTest {
         map.put("categoryName", "师兄最不爱");
         map.put("categoryType", 101);
         int result = productCategoryMapper.insertByMap(map);
+        assertEquals(1, result);
+    }
+
+    @Test
+    @Disabled
+    void insertByObject() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("炸食");
+        productCategory.setCategoryType(102);
+        int result = productCategoryMapper.insertByObject(productCategory);
+        assertEquals(1, result);
+    }
+
+    @Test
+    @Disabled
+    void findByCategoryType() {
+        Integer categoryType = 101;
+        ProductCategory productCategoryResult = productCategoryMapper.findByCategoryType(categoryType);
+        assertNotNull(productCategoryResult);
+    }
+
+    @Test
+    @Disabled
+    void findByCategoryName() {
+        String categoryName = "师兄最不爱";
+        List<ProductCategory> productCategoryList = productCategoryMapper.findByCategoryName(categoryName);
+        assertEquals(2, productCategoryList.size());
+    }
+
+    @Test
+    @Disabled
+    void updateByCategoryType() {
+        String categoryName = "炸食";
+        Integer categoryType = 102;
+        int result = productCategoryMapper.updateByCategoryType(categoryName, categoryType);
         assertEquals(1, result);
     }
 }
